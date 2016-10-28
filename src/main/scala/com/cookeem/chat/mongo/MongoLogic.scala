c64e492d3283ce9e7a3f5e57b9b944dc23091634
cookeem <cookeem@qq.com>
2016-10-28 10:29:00 +0800

    modified:   README.md 	new file:   src/main/scala/com/cookeem/chat/mongo/MongoLogic.scala 	modified:   src/main/scala/com/cookeem/chat/mongo/MongoOps.scala

---
+++ src/main/scala/com/cookeem/chat/mongo/MongoLogic.scala
@@ -0,0 +1,8 @@
+package com.cookeem.chat.mongo
+
+/**
+  * Created by cookeem on 16/10/28.
+  */
+object MongoLogic {
+
+}
--- src/main/scala/com/cookeem/chat/mongo/MongoOps.scala
+++ src/main/scala/com/cookeem/chat/mongo/MongoOps.scala
@@ -2,13 +2,11 @@ package com.cookeem.chat.mongo
 
 import java.util.concurrent.Executors
 
-import play.api.libs.json._
 import reactivemongo.api.collections.bson.BSONCollection
-import reactivemongo.api.commands.Command
+import reactivemongo.api.commands.{Command, UpdateWriteResult}
 import reactivemongo.api.commands.Command.CommandWithPackRunner
 import reactivemongo.api._
 import reactivemongo.bson._
-import reactivemongo.play.json._
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -28,27 +26,96 @@ object MongoOps {
 
   val usersCollection = cookimDB.map(_.collection[BSONCollection](colUsersName))
 
-  def initUsersCollection() = {
-    cookimDB.foreach { db =>
+  def createIndex(colName: String, indexSettings: Array[(String, Int, Boolean)]): Future[String] = {
+    cookimDB.map { db =>
+      var errmsg = ""
       val runner: CommandWithPackRunner[BSONSerializationPack.type] = Command.run(BSONSerializationPack, FailoverStrategy.strict)
-      val commandDoc = BSONDocument(
-        "createIndexes" -> colUsersName,
-        "indexes" -> BSONArray(
+      var indexSettingDoc = BSONArray()
+      indexSettings.foreach { case (indexCol, indexMode, unique) =>
+        indexSettingDoc = indexSettingDoc.add(
           BSONDocument(
-            "key" -> BSONDocument(
-              "login" -> 1
-            ),
-            "name" -> s"index-$colUsersName-login",
-            "unique" -> BSONBoolean(false)
+            "key" -> BSONDocument(indexCol -> indexMode),
+            "name" -> s"index-$colName-$indexCol",
+            "unique" -> BSONBoolean(unique)
           )
         )
+      }
+      val commandDoc = BSONDocument(
+        "createIndexes" -> colName,
+        "indexes" -> indexSettingDoc
       )
       val futureDoc = runner(db, runner.rawCommand(commandDoc)).one[BSONDocument](ReadPreference.primaryPreferred)
-      futureDoc.foreach { doc =>
-        val json = Json.toJson(doc)
-        println(Json.prettyPrint(json))
+      futureDoc.map { doc =>
+        if (doc.get("errmsg").isDefined) {
+          errmsg = doc.getAs[String]("errmsg").getOrElse("")
+        } else {
+          errmsg = ""
+        }
+        errmsg
       }
-    }
+    }.recover { case e: Throwable =>
+      Future(s"${e.getClass}, ${e.getMessage}, ${e.getCause}, ${e.getStackTrace.mkString("\n")}")
+    }.flatMap(f => f)
   }
+
+  def upsertCollection(futureCollection: Future[BSONCollection], selector: BSONDocument, update: BSONDocument, upsert: Boolean): Future[BSONValue] = {
+    futureCollection.map { col =>
+      col.findAndUpdate(
+        selector,
+        update,
+        upsert = upsert
+      ).map{ fr =>
+        var id: BSONValue = BSONNull
+        fr.value match {
+          case Some(frv) =>
+            id = frv.getTry("_id").getOrElse(BSONNull)
+          case _ =>
+            fr.lastError match {
+              case Some(frl) =>
+                frl.upsertedId match {
+                  case Some(v) =>
+                    id = v.asInstanceOf[BSONValue]
+                  case _ =>
+                }
+              case _ =>
+            }
+        }
+        id
+      }
+    }.flatMap(f => f)
+  }
+
+  def findCollection(futureCollection: Future[BSONCollection], selector: BSONDocument, count: Int = -1): Future[List[BSONDocument]] = {
+    futureCollection.map(
+      _.find(selector).cursor[BSONDocument]().collect(count, Cursor.FailOnError[List[BSONDocument]]())
+    ).flatMap(f => f)
+  }
+
+  def pushArrayCollection(futureCollection: Future[BSONCollection], selector: BSONDocument, pushDoc: BSONDocument, upsert: Boolean, multi: Boolean): Future[UpdateWriteResult] = {
+    futureCollection.map(
+      _.update(
+        selector,
+        BSONDocument("$push" -> pushDoc),
+        upsert = true,
+        multi = true
+      )
+    ).flatMap(f => f)
+  }
+
+  def updateCollection(futureCollection: Future[BSONCollection], selector: BSONDocument, update: BSONDocument, upsert: Boolean, multi: Boolean): Future[UpdateWriteResult] = {
+    futureCollection.map(
+      _.update(
+        selector,
+        update,
+        upsert = true,
+        multi = true
+      )
+    ).flatMap(f => f)
+  }
+
+
+
+
+
 
 }
